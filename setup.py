224477b5a638cd8bdac1a4bffed83e3b9c1b3289
OmarCardenass <85803845+OmarCardenass@users.noreply.github.com>
2021-06-12 16:27:26 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+setup(
+    name='prueba',
+    version='1',
+    author='Omar Cardenas',
+    author_email='cardenas.omar.lp@gmail.com',
+    description=('un simple proceso ETL Extract-Transform-Load')
